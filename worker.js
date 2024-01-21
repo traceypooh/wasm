d9bdb17706eb23c571c603c3a2127cb6766c7d15
Tracey Jaquith <tracey@archive.org>
2024-01-21 10:48:30 -0800

    allow demo to live in a subdir.  better automatic picking of networking defaults

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
 importScripts("https://cdn.jsdelivr.net/npm/xterm-pty@0.9.4/workerTools.js");
-importScripts(location.origin + "/browser_wasi_shim/index.js");
-importScripts(location.origin + "/browser_wasi_shim/wasi_defs.js");
-importScripts(location.origin + "/worker-util.js");
-importScripts(location.origin + "/wasi-util.js");
+importScripts("browser_wasi_shim/index.js");
+importScripts("browser_wasi_shim/wasi_defs.js");
+importScripts("worker-util.js");
+importScripts("wasi-util.js");
 
 onmessage = (msg) => {
     if (serveIfInitMsg(msg)) {
@@ -202,7 +202,7 @@ function getNetParam() {
             };
         }
     }
-    return location.host.endsWith('.github.io')
+    return location.hostname === 'localhost' || location.host.endsWith('.github.io')
         ? { mode: "browser", param: undefined } // CORS option
         : { mode: "delegate", param: `wss://${location.hostname}:8080` }
 }
